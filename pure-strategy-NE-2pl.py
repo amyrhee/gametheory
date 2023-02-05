ca16d7b2918bd1ac2357a7543d0e20b8822463af
amyrhee <66448942+amyrhee@users.noreply.github.com>
2023-02-05 14:48:35 -0500

    Update pure-strategy-NE-2pl.py

--- pure-strategy-NE-2pl.py
+++ pure-strategy-NE-2pl.py
@@ -17,13 +17,10 @@ class bimatrix:
         self.n_str = len(row_payoffs)
     
     def pure_ne(self):
-        max_r = np.zeros((self.n_str,1))
-        ind_r = np.zeros((self.n_str,2))
+        max_r, max_c = np.zeros((self.n_str,1)), np.zeros((self.n_str,1))
+        ind_r, ind_c = np.zeros((self.n_str,2)), np.zeros((self.n_str,2))
         
-        max_c = np.zeros((self.n_str,1))
-        ind_c = np.zeros((self.n_str,2))
-        
-        pure_ne = []
+        ne = []
         
         for i in range(self.n_str):
             max_r[i] = np.max(self.mat_r[:,i])
@@ -33,33 +30,23 @@ class bimatrix:
             ind_c[i] = i, np.argmax(self.mat_c[i,:])
             
             if (ind_r[i,:] == ind_c[i,:]).all() == True:
-                pure_ne.append(ind_r[i,:])
-        self.pure_ne = pure_ne
-        return pure_ne
+                ne.append(ind_r[i,:])
+                
+        self.pure_neq = ne
+        
+        if len(ne) == 0:
+            print("No pure NE found.")
+        else:
+            return ne
     
     def pure_ne_payoffs(self):
         big_mat = np.dstack(((self.mat_r, self.mat_c)))
         
         ne_payoffs = []
         
-        for i in range(len(self.pure_ne)):
-            str_ind = list(map(int, self.pure_ne[i]))
+        for i in range(len(self.pure_ne())):
+            str_ind = list(map(int, self.pure_neq[i]))
             ne_payoffs.append(big_mat[str_ind[0], str_ind[1]])
         
         self.ne_payoffs = ne_payoffs
         return ne_payoffs
-    
-    
-# Example with 3 x 3 game
-
-mat_r = np.array([[10, 0, 0], [6, 1, -1], [12, -1, 5]])
-mat_c = np.array([[10, 6, 12], [0, 5, -1], [0, -1, 1]])
-
-gamma = bimatrix(mat_r, mat_c)
-
-# Assuming that we've enumerated the strategies from top (0) to bottom (n) for the row player, and left (0) to right (n) for the column player
-print('The pure NE are the following strategy profiles (row pl. strategy index, column pl. strategy index): ', 
-      gamma.pure_ne())
-
-print('The respective payoffs are: ', gamma.pure_ne_payoffs())
-
